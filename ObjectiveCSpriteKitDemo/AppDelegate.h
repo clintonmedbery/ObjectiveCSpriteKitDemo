bbbb05c1a57f45d4225c80d1bc56785a005995d7
ClintonMedbery <clintomed@gmail.com>
2016-01-27 15:12:28 -0500

    Initial Commit

---
+++ ObjectiveCSpriteKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCSpriteKitDemo
+//
+//  Created by Clinton Medbery on 1/27/16.
+//  Copyright © 2016 Clinton Medbery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveCSpriteKitDemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ObjectiveCSpriteKitDemo
+//
+
+//  Copyright (c) 2016 Clinton Medbery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
